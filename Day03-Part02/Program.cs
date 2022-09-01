7357931f634e8cbc56573de700a2ba16fb11b76c
JuMaxi <jmaximovski@gmail.com>
2022-09-01 19:24:37 +0100

    Implementation Day03-Part02

--- Day03-Part02/Program.cs
+++ Day03-Part02/Program.cs
@@ -10,51 +10,97 @@ namespace Day03_Part02
             string[] ReadText = File.ReadAllLines("C:/Teste/teste.txt");
             int Bit1 = 0;
             int Bit0 = 0;
-           // int Oxygen = 0;
-            //int CO2 = 0;
+            int Oxygen = 0;
+            int CO2 = 0;
+            int PositionChar = 0;
 
 
-            for (int PositionArray = 0; PositionArray <= ReadText.Length - 1; PositionArray++)
+            while (ReadText.Length >= 3)
             {
-                string PositionInitial = ReadText[PositionArray];
-
-
-                    char ValueChar = PositionInitial[0];
-
-                    if (ValueChar == '1')
-                    {
-                        Bit1 = Bit1+ 1;
-                    }
-
-                    Bit0 = ReadText.Length - Bit1;
-                
-            }
-
-
-            // Bit1 = 7,   Bit0 = 5
-            int qualquer = 0;
-            if (Bit1 > Bit0)
-            {
-                string [] teste1 = new string [Bit1];
-                
-                string [] teste2 = new string [Bit0];
-
-                for (int PositionArray = 0; PositionArray <= ReadText.Length-1; PositionArray++)
+                // Descobrir quantidade de bits predominantes na coluna ATUAL
+                for (int PositionArray = 0; PositionArray <= ReadText.Length - 1; PositionArray++)
                 {
                     string PositionInitial = ReadText[PositionArray];
-                    char ValueChar = PositionInitial[0];
+
+                    char ValueChar = PositionInitial[PositionChar];
 
                     if (ValueChar == '1')
                     {
-                        teste1[qualquer] = PositionInitial;
-                        qualquer = qualquer + 1;
+                        Bit1 = Bit1 + 1;
                     }
-
+                    else
+                    {
+                        Bit0 = Bit0 + 1;
+                    }
                 }
 
+
+                string[] NewReadText = new string[0];
+
+                if (Bit1 > (ReadText.Length / 2))
+                {
+                    NewReadText = new string[Bit1];
+                }
+                else
+                {
+                    if (Bit0 > (ReadText.Length / 2))
+                    {
+                        NewReadText = new string[Bit0];
+                    }
+                }
+
+
+
+                int NewPositionArray = 0;
+
+
+                for (int PositionArray = 0; PositionArray <= ReadText.Length - 1; PositionArray++)
+                {
+                    string PositionInitial = ReadText[PositionArray];
+                    char ValueChar = PositionInitial[PositionChar];
+
+                    if (Bit1 > (ReadText.Length / 2) && ValueChar == '1')
+                    {
+                        NewReadText[NewPositionArray] = PositionInitial;
+                        NewPositionArray = NewPositionArray + 1;
+                    }
+                    else
+                    {
+                        if (Bit0 > (ReadText.Length / 2) && ValueChar == '0')
+                        {
+                            NewReadText[NewPositionArray] = PositionInitial;
+                            NewPositionArray = NewPositionArray + 1;
+                        }
+                    }
+                }
+
+                PositionChar = PositionChar + 1;
+                ReadText = NewReadText;
+                Bit0 = 0;
+                Bit1 = 0;
+                string teste = " ";
+
+                if (ReadText.Length == 2)
+                    for (int PositionArrayEnd = 0; PositionArrayEnd <= ReadText.Length - 1; PositionArrayEnd++)
+                    {
+                        string verificacao = ReadText[PositionArrayEnd];
+                        char EndChar = verificacao[PositionChar];
+
+                        if (EndChar == '1')
+                        {
+                            teste = verificacao;
+                        }
+
+                    }
             }
-                   
-                       
+
+
+
+
+
+
+
+
 
 
             //int PowerConsumption = GamaRate * EpsilonRate;
@@ -64,3 +110,4 @@ namespace Day03_Part02
         }
     }
 }
+
